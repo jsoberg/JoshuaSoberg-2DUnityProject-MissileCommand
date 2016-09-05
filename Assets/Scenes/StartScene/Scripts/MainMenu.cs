457b3438595ead210f1f3c356ecbba2127a81173
Joshua Soberg <j15soberg@gmail.com>
2016-09-05 12:03:04 -0400

    - Added initial background texture and a main menu script.

---
+++ Assets/Scenes/StartScene/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public Texture BackgroundTexture;
+
+    void OnGUI()
+    {
+        // Load background texture.
+        Rect screenRect = new Rect(0,0,Screen.width, Screen.height);
+        GUI.DrawTexture(screenRect, BackgroundTexture);
+    }
+}

5890db79eab0ef744c51b87d05c4cfa3ef6836ac
Joshua Soberg <j15soberg@gmail.com>
2016-09-13 20:27:12 -0400

    - Set up project to run on Android. Implement stub input controller.

---
+++ Assets/Scenes/MainScene/Scripts/InputController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputController : MonoBehaviour 
+{
+    void Start()
+    {
+		
+    }
+
+	void Update () 
+	{
+		// Native android touch events
+		foreach (Touch touch in Input.touches) {
+			HandleTouch(touch.fingerId, Camera.main.ScreenToWorldPoint(touch.position), touch.phase);
+		}
+
+		// Simulated touch events from mouse events
+		if (Input.touchCount == 0) {
+			if (Input.GetMouseButtonDown(0) ) {
+				HandleTouch(10, Camera.main.ScreenToWorldPoint(Input.mousePosition), TouchPhase.Began);
+			} else if (Input.GetMouseButton(0) ) {
+				HandleTouch(10, Camera.main.ScreenToWorldPoint(Input.mousePosition), TouchPhase.Moved);
+			} else if (Input.GetMouseButtonUp(0) ) {
+				HandleTouch(10, Camera.main.ScreenToWorldPoint(Input.mousePosition), TouchPhase.Ended);
+			}
+		}
+	}
+
+	private void HandleTouch(int touchFingerId, Vector3 touchPosition, TouchPhase touchPhase) 
+	{
+		switch (touchPhase) {
+		case TouchPhase.Began:
+			// TODO
+			break;
+		case TouchPhase.Moved:
+			// TODO
+			break;
+		case TouchPhase.Ended:
+			// TODO
+			break;
+		}
+	}
+}

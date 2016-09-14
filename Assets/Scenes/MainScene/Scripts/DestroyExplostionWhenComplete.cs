e54ab8cf264404962d4349bbcaab353daf4f0bd4
Joshua Soberg <j15soberg@gmail.com>
2016-09-14 19:35:21 -0400

    - Added explosion audio to explosion, and made sure the explosion destroyed itself once complete.

---
+++ Assets/Scenes/MainScene/Scripts/DestroyExplostionWhenComplete.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyExplostionWhenComplete : MonoBehaviour 
+{
+    public Animator ExplosionAnimator;
+    	
+    void Start()
+    {
+        ExplosionAnimator = ExplosionAnimator.GetComponent<Animator>();
+    }
+
+	void Update () 
+    {
+        AnimatorStateInfo asi = ExplosionAnimator.GetCurrentAnimatorStateInfo(0);
+
+        if (asi.normalizedTime >= 1) {
+            Destroy(gameObject);
+        }
+	}
+}

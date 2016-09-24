44f9ff7073b6711af6f5bdf145e8b6790cd527a5
Joshua Soberg <j15soberg@gmail.com>
2016-09-24 14:48:59 -0400

    - Added cities, and scaled back the missiles a bit.

---
+++ Assets/_Scenes/GameScene/Scripts/CityController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityController : MonoBehaviour
+{
+    public GameObject CitySprite;
+
+    public GameObject BrokeCitySprite;
+    public GameObject BurningCityParticleSystem;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        RazeCity();
+    }
+
+    private void RazeCity()
+    {
+        CitySprite.SetActive(false);
+        GetComponent<PolygonCollider2D>().enabled = false;
+
+        BrokeCitySprite.SetActive(true);
+        BurningCityParticleSystem.SetActive(true);
+    }
+}

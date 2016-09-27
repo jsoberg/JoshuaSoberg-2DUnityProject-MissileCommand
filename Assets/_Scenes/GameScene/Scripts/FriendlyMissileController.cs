2d2cb21f7fadcb1b8ffa84c4e3e8e0ebe8b3f606
Joshua Soberg <j15soberg@gmail.com>
2016-09-26 22:31:34 -0400

    - Added more precise friendly missile explosions.

--- Assets/_Scenes/GameScene/Scripts/FriendlyMissileController.cs
+++ Assets/_Scenes/GameScene/Scripts/FriendlyMissileController.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class FriendlyMissileController : MissileController
 {
+    protected override bool HasReachedTarget()
+    {
+        // We've reached our target if we reach the height that we initially targeted.
+        return base.HasReachedTarget() || (transform.position.y > TargetPosition.y);
+    }
+
     protected override void PostStart()
     {
         Object[] friendlies = Object.FindObjectsOfType(typeof(Friendly));
